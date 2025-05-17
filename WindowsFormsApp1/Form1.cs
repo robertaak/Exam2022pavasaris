6fe5d27024623d401945745fdb352649f1da8b26
robertaak <r.kimerale@gmail.com>
2025-05-17 10:17:07 +0300

    Kimerale Pb-It-N-2 Eksamens 17.05.25.

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -2,19 +2,63 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        IWebDriver driver;
         public Form1()
         {
             InitializeComponent();
+            this.Load += OpenWebsite;
+        }
+
+        private void OpenWebsite(object sender, EventArgs e)
+        {
+            var options = new ChromeOptions();
+            options.AddExcludedArgument("enable-logging");
+            // IMPORTANT! Must add path to own chrome driver location!
+            driver = new ChromeDriver(@"C:\Users\rkime\OneDrive\Dators\", options);
+            driver.Url = "https://ebay.com";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string searchText = textBox1.Text;
+
+            var searchBox = driver.FindElement(By.Id("gh-ac"));
+            var searchBtn = driver.FindElement(By.Id("gh-search-btn"));
+
+            searchBox.Clear();
+            searchBox.SendKeys(searchText);
+            searchBtn.Click();
+
+            string resultUrl = driver.Url;
+            textBox2.Text = resultUrl;
+
+            richTextBox1.AppendText(resultUrl + Environment.NewLine);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            driver.Quit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            driver.Navigate().Back();
+            textBox1.Clear();
+            textBox2.Clear();
         }
     }
 }
---
+++ WindowsFormsApp1/Tests/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace WindowsFormsApp1.Tests
+{
+    internal class Tests
+    {
+        [Test]
+        public void Test1_field()
+        {
+            var options = new ChromeOptions();
+            options.AddExcludedArgument("enable-logging");
+            // IMPORTANT! Must add path to own chrome driver location!
+            IWebDriver driver = new ChromeDriver(@"C:\Users\rkime\OneDrive\Dators\", options);
+            driver.Url = "https://ebay.com";
+            driver.FindElement(By.Id("gh-ac"));
+        }
+
+        public void Test2_search()
+        {
+            var options = new ChromeOptions();
+            options.AddExcludedArgument("enable-logging");
+            // IMPORTANT! Must add path to own chrome driver location!
+            IWebDriver driver = new ChromeDriver(@"C:\Users\rkime\OneDrive\Dators\", options);
+            driver.Url = "https://ebay.com";
+            driver.FindElement(By.Id("gh-search-btn"));
+        }
+    }
+}
